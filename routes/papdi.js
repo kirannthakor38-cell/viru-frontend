3c2a5bfbdd4b2f7f24ac5e7475705f1ea52a8125
Thakor Kiran <kirannthakor38@gmail.com>
2026-01-29 16:58:02 +0530

    Update papdi.js

--- routes/papdi.js
+++ routes/papdi.js
@@ -5,6 +5,7 @@ import {
     createPapdi,
     updatePapdi,
     deletePapdi,
+    getPapdiImage,
 } from '../controllers/papdiController.js';
 import { protect, requireAdmin } from '../middlewares/auth.js';
 import upload from '../middlewares/upload.js';
@@ -12,6 +13,7 @@ import upload from '../middlewares/upload.js';
 const router = express.Router();
 
 router.get('/', getAllPapdi);
+router.get('/image/:id', getPapdiImage);
 router.get('/:id', getPapdiById);
 router.post('/', protect, requireAdmin, upload.single('image'), createPapdi);
 router.put('/:id', protect, requireAdmin, upload.single('image'), updatePapdi);
